960d33ac21a56cae551bf4857143c10518dfcf57
Angsthase <mikieburrow@gmail.com>
2025-10-06 12:29:34 +0200

    Update next.config.js

--- the-turnstile-nextjs/next.config.js
+++ the-turnstile-nextjs/next.config.js
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
   output: 'export',
   images: { unoptimized: true },
   reactStrictMode: true,
-  // Skip lint + TS in CI so the build can complete
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  typescript: { ignoreBuildErrors: true }
 };
