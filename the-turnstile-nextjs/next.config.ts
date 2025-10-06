71cc199e4dc86ce831ff2111c564b78e64f5751c
Angsthase <mikieburrow@gmail.com>
2025-10-06 11:09:54 +0200

    Update and rename next.config.ts to next.config.js

--- the-turnstile-nextjs/next.config.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',          // static export for Firebase Hosting
-  images: { unoptimized: true }, // required for next export if you use <Image>
-  reactStrictMode: true
-};
-module.exports = nextConfig;
